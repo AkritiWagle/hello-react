969425c57803ade1785b566cf3a51bb4646f7ef5
AkritiWagle <aakritiwagle10@gmai.com>
2024-07-22 07:17:40 +0545

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HelloReact from './HelloReact';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <HelloReact />
+    </div>
+  );
+};
+
+export default App;
